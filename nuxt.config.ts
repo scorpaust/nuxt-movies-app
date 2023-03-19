f8ef7cb14146d6812f31c8a0998b63c36da453c7
Dinis Costa <scorpaust@hotmail.com>
2023-03-19 03:27:35 +0000

    Environment Vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    publicRuntimeConfig: {
-        omdbKey: ''
+    runtimeConfig: {
+        public: {
+            omdbKey: ''
+        }
     },
     modules: [
         ["@pinia/nuxt", {
