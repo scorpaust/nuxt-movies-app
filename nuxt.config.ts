303930651d14319beced06da57786245024c9076
Dinis Costa <scorpaust@hotmail.com>
2023-03-20 00:18:33 +0000

    Environment Vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig: {
-        public: {
-            omdbKey: process.env.NUXT_OMDB_KEY
-        }
+
+        omdbKey: ''
+
     },
     modules: [
         ["@pinia/nuxt", {
--- server/api/omdb.get.ts
+++ server/api/omdb.get.ts
@@ -2,7 +2,9 @@ export default defineEventHandler(async (event) => {
 
     const { query } = getQuery(event);
 
-    const Search: Promise<any> = await $fetch<Promise<any>>(`https://www.omdbapi.com/?apikey=bac43bed&s=${query}`)
+    const config = useRuntimeConfig();
+
+    const Search: Promise<any> = await $fetch<Promise<any>>(`https://www.omdbapi.com/?apikey=${config.apiKey}&s=${query}`)
 
     return Search;
   })
